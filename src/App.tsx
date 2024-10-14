3935a2aaeb0cd8446c9036589faeb27346161e66
Nuri H <1554735+nurih@users.noreply.github.com>
2024-10-14 14:16:21 -0700

    Add QR part of edit area, under toggle.

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,7 @@ function App() {
     const list = [{ id: Date.now(), text: txt, name: (name || txt) } as QrData, ...items];
     setItems(list)
     setTxt('https://');
+    setName('');
     setOpen('0');
     store.save(list);
   }
@@ -69,7 +70,7 @@ function App() {
       <div className='jumbotron'>
         <p className="fs-2 mb-2 align-center title">
           <img src='shareme.svg' style={{ height: '2rem' }} />
-            Cher Ami
+          Cher Ami
         </p>
 
         <p>Generate, save, and share your QR codes with ease! Free to use, works offline, no signup.</p>
@@ -97,42 +98,43 @@ function App() {
       </Accordion>
 
       <p />
-      <p>Enter a URL, name it, and it creates a QR for you.</p>
-      <Form style={{ padding: '1rem', backgroundColor: '#F8F9FA' }}>
-        <FormGroup>
-          <Label for="title">Name</Label>
-          <Input
-            id="title"
-            name="title"
-            placeholder="Name for this QR..."
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="url">Url</Label>
-          <Input id="url" name="url" placeholder="https://..." type="url" value={txt} valid={urlRe.test(txt)}
-            onChange={e => setTxt(e.target.value)}
-          />
-        </FormGroup>
-        <Button onClick={addItem}
-          disabled={!urlRe.test(txt)}
-          color={urlRe.test(txt) ? 'success' : 'secondary'}><i className='bi bi-floppy'></i></Button>
-      </Form>
-
-      <p />
       <Card>
+        <Form>
 
-        <FormGroup switch>
-          <Label >
-            {editMode ? (<p className='text-warning'>Editing</p>) : (<p className='text-info'>Edit my list</p>)}
-          </Label>
-          <Input type="switch" role="switch" onChange={(e) => setEditMode(e.target.checked)} />
+          <FormGroup switch>
+            <Label >
+              {editMode ? (<p className='text-warning'>Editing</p>) : (<p className='text-info'>Edit my list</p>)}
+            </Label>
+            <Input type="switch" role="switch" onChange={(e) => setEditMode(e.target.checked)} />
+          </FormGroup>
+        </Form>
 
-
-        </FormGroup>
         {editMode &&
           <>
+            <p>Enter a URL, name it, and it creates a QR for you.</p>
+            <Form style={{ padding: '1rem', backgroundColor: '#F8F9FA' }}>
+              <FormGroup>
+                <Label for="title">Name</Label>
+                <Input
+                  id="title"
+                  name="title"
+                  placeholder="Name for this QR..."
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="url">Url</Label>
+                <Input id="url" name="url" placeholder="https://..." type="url" value={txt} valid={urlRe.test(txt)}
+                  onChange={e => setTxt(e.target.value)}
+                />
+              </FormGroup>
+              <Button onClick={addItem}
+                disabled={!urlRe.test(txt)}
+                color={urlRe.test(txt) ? 'success' : 'secondary'}><i className='bi bi-floppy'></i></Button>
+            </Form>
+
+
             <ListEditor items={items} updateItems={saveItems} />
           </>
         }
