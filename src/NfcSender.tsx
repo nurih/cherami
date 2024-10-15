73f8454cc1da60476bee316f3132f28a309c0a64
Nuri H <1554735+nurih@users.noreply.github.com>
2024-10-15 15:05:26 -0700

    NFC Send

---
+++ src/NfcSender.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Button, Toast, ToastBody, ToastHeader } from "reactstrap";
+
+
+function NfcWriter({ url }: { url: string }) {
+  const [messages, setMessages] = useState<string[]>([]);
+  let nfc: any;
+
+  if ('NDEFReader' in window) {
+    nfc = new (window as any).NDEFReader();
+  } else {
+    nfc = null;
+  }
+
+
+  const send = async () => {
+    try {
+      const record = { recordType: 'url', data: url, };
+
+      setMessages([...messages, 'Starting scan']);
+
+      await nfc.scan();
+
+      setMessages([...messages, 'Scanning...']);
+
+      nfc.onreading = async function* r() {
+        setMessages([...messages, 'Found!', `Sending ${url}`]);
+
+        await this.nfc.write(record);
+        setMessages([...messages, `Sent ${url}.`]);
+      };
+
+    } catch (error) {
+      console.error('Error writing NFC message:', error);
+    }
+  }
+
+
+  return (
+    <>
+      <Button color="info" onClick={send} disabled={nfc == null}><i className="bi bi-wifi "></i></Button>
+      <div className="toast-container">
+        {
+          messages.map((m, i) => (
+            <Toast key={i} toggle={function noRefCheck() { }}>{m}</Toast>
+          ))
+        }
+      </div>
+    </>
+
+  )
+}
+
+export default NfcWriter;
--- src/QrTile.tsx
+++ src/QrTile.tsx
@@ -1,15 +1,20 @@
 import QRCode from "react-qr-code";
 import { Nav, NavItem, NavLink } from "reactstrap";
+import NfcSender from "./NfcSender";
 
 
 export default function QrTile({ data }: { data: string }) {
   return (
-    <div>      
-      <QRCode value={data} level='L'/>
+    <div>
+      <QRCode value={data} level='L' />
       <Nav>
         <NavItem>
           <NavLink href={data}>{data}</NavLink>
         </NavItem>
       </Nav>
+      <div className="align-self-end">
+
+        <NfcSender url={data} />
+      </div>
     </div>);
 }
